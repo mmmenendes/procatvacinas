439ab792f07585345722fd4cb9270f97f58e5d28
lucas <lucasgfmoraes@gmail.com>
2018-08-17 22:06:09 -0300

    initial git

---
+++ SITE/js/pagesApoio.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){    
+    $(".head-wrapper").css("background-color","#a6dad7");
+    $("#menu-social-toggle").css("display","none");
+
+    $("#menu-toggle").click(function(e) {
+        $(".head-wrapper").css("background-color","#a6dad7");
+    });
+});
+
+
