cd7a916f6895becb085b67ba9e88fc6780915858
melissa <mmmenendes@gmail.com>
2018-09-08 15:01:21 -0300

    atualização criação modal

--- js/procatEventsUtils.js
+++ js/procatEventsUtils.js
@@ -63,23 +63,63 @@ function sideMenuButtons(elementMenuClick, elementMenuToggle){
         elementMenuToggle.toggleClass("toggled");
     })
 }
+
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////// MAPS ////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
 function init_map(gmapElement, latitude, longitude, text){
-		var myOptions = {
-			zoom:10,
-			center:new google.maps.LatLng(latitude, longitude),
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-			
-		map = new google.maps.Map(document.getElementById(gmapElement), myOptions);
-		marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(latitude, longitude)});
-		infowindow = new google.maps.InfoWindow({content:text});
-		google.maps.event.addListener(marker, 'click', function(){
-			infowindow.open(map,marker);
-		});
-
+	var myOptions = {
+		zoom:10,
+		center:new google.maps.LatLng(latitude, longitude),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+		
+	map = new google.maps.Map(document.getElementById(gmapElement), myOptions);
+	marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(latitude, longitude)});
+	infowindow = new google.maps.InfoWindow({content:text});
+	google.maps.event.addListener(marker, 'click', function(){
 		infowindow.open(map,marker);
-	}
+	});
+
+	infowindow.open(map,marker);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// MAIL ////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+function sendEmail(mailParamJSON){			
+	//var service_id = "default_service";
+	var service_id = "gmail"
+	var template_id = "template_FyklCJMe";
+	var user_id = "user_0cyWuRROWnRS8TWFmiaQw"
+
+	emailjs.send(service_id,template_id, mailParamJSON, user_id).then(function(response) {
+		console.log('SUCCESS!', response.status, response.text);
+	}, function(error) {
+		console.log('FAILED...', error);
+	});
+}
+
+	/*
+	var data = {
+	    service_id: 'gmail',
+	    template_id: 'template_FyklCJMe',
+	    user_id: 'user_0cyWuRROWnRS8TWFmiaQw',
+	    template_params: {
+	        'username': 'James',
+	        'g-recaptcha-response': '03AHJ_ASjnLA214KSNKFJAK12sfKASfehbmfd...'
+	    }
+	};
+	 
+	$.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+	    type: 'POST',
+	    data: JSON.stringify(data),
+	    contentType: 'application/json'
+	}).done(function() {
+	    alert('Your mail is sent!');
+	}).fail(function(error) {
+	    alert('Oops... ' + JSON.stringify(error));
+	});
+	*/
